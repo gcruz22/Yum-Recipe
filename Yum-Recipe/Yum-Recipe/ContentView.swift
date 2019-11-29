972970037aa53dc8216c2e198f3a9ef2eed9920f
gcruz22 <cruzgilberto54@icloud.com>
2019-11-28 23:45:05 -0700

    created file

---
+++ Yum-Recipe/Yum-Recipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yum-Recipe
+//
+//  Created by gilberto cruz on 11/28/19.
+//  Copyright © 2019 gilberto cruz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
